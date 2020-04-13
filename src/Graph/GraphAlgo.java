fd2915a80fabb7ff4a79e9f8236d79a4aa0d0290
Pratyush Harsh <pratyushharsh2015@gmail.com>
2020-04-13 13:37:59 +0530

    DFS

--- src/Graph/GraphAlgo.java
+++ src/Graph/GraphAlgo.java
@@ -11,35 +11,57 @@ public class GraphAlgo<T> {
         StringBuilder sb = new StringBuilder();
         q.add(start);
 
+        visited.add(start);
         while (q.peek() != null) {
             T head = q.peek();
-            visited.add(head);
             sb.append(head.toString() + " ");
             q.poll();
             for (T el: g.getEdges(head)) {
-                if (!visited.contains(el)) q.add(el);
+                if (!visited.contains(el)) {
+                    visited.add(el);
+                    q.add(el);
+                }
             }
         }
         System.out.println(sb.toString());
     }
 
-    public void dfs(Graph<T> g) {
+    public void dfs(Graph<T> g, T start) {
+        Stack<T> stack = new Stack<>();
+        Set<T> visited = new HashSet<>();
+        stack.push(start);
+        visited.add(start);
 
+        StringBuilder builder = new StringBuilder();
+
+        while (!stack.empty()) {
+            T t = stack.pop();
+            builder.append(t.toString() + " ");
+            for (T egde: g.getEdges(t)) {
+                if (!visited.contains(egde)) {
+                    visited.add(egde);
+                    stack.push(egde);
+                }
+            }
+        }
+        System.out.println(builder.toString());
     }
 
     public static void main(String[] args) {
         GraphAlgo<Integer> gs = new GraphAlgo<Integer>();
-        Graph<Integer> g = new Graph<>();
+        Graph<Integer> g = new Graph<Integer>();
 
-        g.addEdge(0, 1, true);
-        g.addEdge(0, 4, true);
-        g.addEdge(1, 3, true);
-        g.addEdge(1, 4, true);
         g.addEdge(1, 2, true);
-        g.addEdge(3, 4, true);
-        g.addEdge(2, 3, true);
+        g.addEdge(1, 3, true);
+        g.addEdge(2, 5, true);
+        g.addEdge(2, 4, true);
+        g.addEdge(4, 5, true);
+        g.addEdge(4, 6, true);
+        g.addEdge(6, 5, true);
+        g.addEdge(3, 5, true);
 
-        gs.bfs(g, 0);
+        gs.bfs(g, 1 );
+        gs.dfs(g, 1);
         // System.out.println(g.getEdgeCount(true));
     }
 }
